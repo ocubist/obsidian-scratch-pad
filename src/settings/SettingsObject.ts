9627d584b7d54f414b42ae3ac0f3f2627af2d345
ocubist <1m4w0rk3r@gmail.com>
2023-01-20 15:43:53 -0500

    Add<`SettingsObject`> new fields and add them to the `SettingsTab`

--- src/settings/SettingsObject.ts
+++ src/settings/SettingsObject.ts
@@ -1,7 +1,15 @@
 export interface SettingsObject {
-	test: boolean;
+	deleteTrashNotesOnTabClose: boolean;
+	deleteTrashNotesOnStartup: boolean;
+	trashNotePattern: string;
+	trashNoteFolderPath: string;
+	recursive: boolean;
 }
 
 export const DEFAULT_SETTINGS: SettingsObject = {
-	test: false,
+	deleteTrashNotesOnTabClose: false,
+	deleteTrashNotesOnStartup: false,
+	trashNotePattern: "^Untitled",
+	trashNoteFolderPath: "/",
+	recursive: false,
 };
--- src/settings/SettingsTab.ts
+++ src/settings/SettingsTab.ts
@@ -1,6 +1,6 @@
 import DeleteTrashNotesPlugin from "main";
 import { PluginSettingTab, Setting } from "obsidian";
-// import { getFolders } from "src/utility/obsidian-development-tools";
+import { getFolders } from "src/helpers/getFolders";
 
 export class SettingsTab extends PluginSettingTab {
 	plugin: DeleteTrashNotesPlugin;
@@ -15,13 +15,67 @@ export class SettingsTab extends PluginSettingTab {
 
 		containerEl.innerHTML = "";
 
-		new Setting(containerEl).setName("test").addToggle((toggle) => {
-			toggle
-				.setValue(this.plugin.settings.test)
-				.onChange(async (value) => {
-					this.plugin.settings.test = value;
-					await this.plugin.saveSettings();
+		new Setting(containerEl)
+			.setName("Delete Trash-Notes on Tab-Close")
+			.addToggle((toggle) => {
+				toggle
+					.setValue(this.plugin.settings.deleteTrashNotesOnTabClose)
+					.onChange(async (value) => {
+						this.plugin.settings.deleteTrashNotesOnTabClose = value;
+						await this.plugin.saveSettings();
+					});
+			});
+
+		new Setting(containerEl)
+			.setName("Delete Trash-Notes on Start-Up")
+			.addToggle((toggle) => {
+				toggle
+					.setValue(this.plugin.settings.deleteTrashNotesOnStartup)
+					.onChange(async (value) => {
+						this.plugin.settings.deleteTrashNotesOnStartup = value;
+						await this.plugin.saveSettings();
+					});
+			});
+
+		new Setting(containerEl)
+			.setName("RegExp to identify Trash-Notes")
+			.addText((text) => {
+				text.setValue(this.plugin.settings.trashNotePattern).onChange(
+					async (value) => {
+						this.plugin.settings.trashNotePattern = value;
+						await this.plugin.saveSettings();
+					}
+				);
+			});
+
+		new Setting(containerEl)
+			.setName("Folders to look for Trash-Notes")
+			.addDropdown((dropdown) => {
+				const folders = getFolders().sort((a, b) =>
+					a.path.localeCompare(b.path)
+				);
+				folders.forEach((folder) => {
+					// @ts-ignore
+					dropdown.addOption(folder.path, folder.path);
 				});
-		});
+
+				dropdown.setValue(this.plugin.settings.trashNoteFolderPath);
+				dropdown.onChange(async (value) => {
+					this.plugin.settings.trashNoteFolderPath = value;
+					await this.plugin.saveSettings();
+					console.log(`Folder changed: ${value}`);
+				});
+			});
+
+		new Setting(containerEl)
+			.setName("Delete Trash-Notes in Sub-Folders")
+			.addToggle((toggle) => {
+				toggle
+					.setValue(this.plugin.settings.recursive)
+					.onChange(async (value) => {
+						this.plugin.settings.recursive = value;
+						await this.plugin.saveSettings();
+					});
+			});
 	}
 }
