6295ffc453e80c5f5e541799ee120317da1b0734
ocubist <1m4w0rk3r@gmail.com>
2023-01-20 16:35:48 -0500

    Add<`deleteTrashNotes`> toasty with the number of deleted Trash-Notes

--- src/features/deleteTrashNotes.ts
+++ src/features/deleteTrashNotes.ts
@@ -1,11 +1,14 @@
-import { TFile } from "obsidian";
+import { Notice, TFile } from "obsidian";
 import { findTrashNotes } from "./findTrashNotes";
 
 export async function deleteTrashNotes() {
 	const trashNotes = findTrashNotes();
+	const length = trashNotes.length;
 
 	while (trashNotes.length > 0) {
 		const noteToDelete = trashNotes.pop() as TFile;
 		app.vault.delete(noteToDelete); // idea: Maybe better not completely delete them...
 	}
+
+	new Notice(`${length} Trash-Notes have been deleted...`);
 }
